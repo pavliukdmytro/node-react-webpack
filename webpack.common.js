292ae0be419ffed5716a85ee5e4addb0cfc2f9b4
pavliuk <Dima>
2020-02-04 14:34:01 +0200

    add runtime for babel, forever and concurrently

--- webpack.common.js
+++ webpack.common.js
@@ -21,7 +21,8 @@ module.exports = {
 					loader: 'babel-loader',
 					options: {
 						presets: ['@babel/preset-env', '@babel/preset-react'],
-						plugins: ['@babel/plugin-syntax-dynamic-import']
+						plugins: ['@babel/plugin-syntax-dynamic-import',
+							["@babel/plugin-transform-runtime",{"regenerator": true}]]
 					}
 				}
 			},
